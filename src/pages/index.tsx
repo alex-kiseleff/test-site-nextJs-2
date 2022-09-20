030a63b382fdb4133fc0db296d3bc7b2fea16a67
Alexey <64145913+alex-kiseleff@users.noreply.github.com>
2022-09-20 17:28:24 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const MainComponent: NextPage<IProps> = ({ panelsServices }) => {
 
 export const getStaticProps: GetStaticProps = async () => {
 	try {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/db`);
+		const response = await fetch(`${process.env.API_HOST}/api/db`);
 		const data = await response.json();
 
 		if (data.errors) {
